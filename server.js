8388a8da71ab14bd5efe3657c622cc255f3688dd
christian leguiz <christianleguiz13@gmail.com>
2023-03-30 19:53:15 +0800

    try to play in env

--- server.js
+++ server.js
@@ -41,4 +41,4 @@ app.put("/notes/:id", requireAuth, noteController.updateNote)
 app.delete("/notes/:id", requireAuth, noteController.deleteNote)
 
 //start our server
-app.listen(process.env.PORT)
+app.listen(process.env.PORT || 3001)
